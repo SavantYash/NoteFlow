8c571dbe427927038b6e1bf02c09f8490b448d87
SavantYash <yashsavant2004@gmail.com>
2025-01-06 19:04:11 +0530

    Delete ParkEasy directory

--- ParkEasy/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParkeasyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ParkEasy'
--- ParkEasy/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render,redirect
-from ParkEasy.Templates import *
-
-# Create your views here.
-def index(request):
-    return render(request,'index1.html')
